dc1ded9b8a96a3402f59413d7000b94eca0dfadc
Joaquin Navarro <inavarrojoaquin@gmail.com>
2023-03-17 08:37:13 +0100

    Refactored adding Products in ShoppingCart

---
+++ Products.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace ShoppingCartApp
+{
+    internal class Products
+    {
+        List<Product> products;
+
+        public Products()
+        {
+            products = new List<Product>();
+        }
+
+        internal void AddProduct(Product product)
+        {
+            if (products.Exists(x => x.Name == product.Name))
+            {
+                Product findedProduct = products.First(x => x.Name == product.Name);
+                findedProduct.Quantity++;
+                return;
+            }
+
+            products.Add(product);
+        }
+
+        internal string PrintProducts()
+        {
+            if (!products.Any())
+                return "No products";
+
+            StringBuilder productList = new();
+            productList.AppendLine("Products: ");
+            products.ForEach(x => productList.AppendLine(string.Format("-> Name: {0} \t| Price: {1} \t| Quantity: {2}", x.Name, x.Price, x.Quantity)));
+
+            return productList.ToString();
+        }
+
+        internal string PrintTotalOfProducts()
+        {
+            return string.Format("Total of products: {0}", products.Sum(x => x.Quantity));
+        }
+
+        internal string PrintTotalPrice()
+        {
+            return string.Format("Total price: {0}", Math.Round(products.Sum(x => x.Price * x.Quantity), 2));
+        }
+    }
+}
--- ShoppingCart.cs
+++ ShoppingCart.cs
@@ -4,60 +4,26 @@ namespace ShoppingCartApp
 {
     internal class ShoppingCart : IShoppingCart
     {
-        private List<Product> products;
+        private Products products;
 
         public ShoppingCart()
         {
-            products = new List<Product>();
+            products = new Products();
         }
 
         public void AddProducts(List<Product> productsToAdd)
         {
             List<Product> clonesProductsToAdd = productsToAdd.ConvertAll(x => x.Clone());
-            clonesProductsToAdd.ForEach(x => AddProduct(x));
-        }
-
-        private void AddProduct(Product product)
-        {
-            if (products.Exists(x => x.Name == product.Name))
-            {
-                Product findedProduct = products.First(x => x.Name == product.Name);
-                findedProduct.Quantity++;
-                return;
-            }
-
-            products.Add(product);
-        }
-
-        private string PrintProducts()
-        {
-            if (!products.Any())            
-                return "No products";
-
-            StringBuilder productList = new();
-            productList.AppendLine("Products: ");
-            products.ForEach(x => productList.AppendLine(string.Format( "-> Name: {0} \t| Price: {1} \t| Quantity: {2}", x.Name, x.Price, x.Quantity )));
-
-            return productList.ToString();
-        }
-
-        private string PrintTotalOfProducts()
-        {
-            return string.Format("Total of products: {0}", products.Sum(x => x.Quantity));
-        }
-
-        private string PrintTotalPrice()
-        {
-            return string.Format("Total price: {0}", Math.Round(products.Sum(x => x.Price * x.Quantity), 2));
+            clonesProductsToAdd.ForEach(x => products.AddProduct(x));
         }
 
         public string PrintShoppingCart()
         {
             StringBuilder shoppingCartBuilder = new();
-            shoppingCartBuilder.AppendLine(PrintProducts());
+            shoppingCartBuilder.AppendLine(products.PrintProducts());
             shoppingCartBuilder.AppendLine("No promotion");
-            shoppingCartBuilder.AppendLine(PrintTotalOfProducts());
-            shoppingCartBuilder.AppendLine(PrintTotalPrice());
+            shoppingCartBuilder.AppendLine(products.PrintTotalOfProducts());
+            shoppingCartBuilder.AppendLine(products.PrintTotalPrice());
 
             return shoppingCartBuilder.ToString();
         }
