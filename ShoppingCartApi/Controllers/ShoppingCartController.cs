4aa5d0b37c36b3d3a4b24d50e1cb21fd3997dcbc
Joaquin Navarro <inavarrojoaquin@gmail.com>
2023-05-25 20:51:27 +0200

    Updated ShoppingCartController with AddProduct and DeleteProduct

--- ShoppingCartApi/Controllers/ShoppingCartController.cs
+++ ShoppingCartApi/Controllers/ShoppingCartController.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ShoppingCartApp.App.Infrastructure;
 using ShoppingCartApp.App.UseCases.AddProduct;
-using ShoppingCartApp.App.UseCases.ApplyDiscount;
 using ShoppingCartApp.App.UseCases.DeleteProduct;
 using ShoppingCartApp.App.UseCases.PrintShoppingCart;
 using ShoppingCartApp.DTOs;
 using ShoppingCartApp.Shared.Domain;
-using ShoppingCartAppTest.App.UseCases.AddProduct;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ShoppingCartApi.Controllers
 {
@@ -16,62 +11,37 @@ namespace ShoppingCartApi.Controllers
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
-        private ShoppingCartDbContext context;
-        private IShoppingCartRepository shoppingCartRepository;
+        private readonly ICommandBus commandBus;
         private readonly IQueryBus queryBus;
 
-        public ShoppingCartController(IQueryBus queryBus)
+        public ShoppingCartController(ICommandBus commandBus, IQueryBus queryBus)
         {
-            context = new ShoppingCartDbContext();
-            shoppingCartRepository = new ShoppingCartRepository(context);
+            this.commandBus = commandBus;
             this.queryBus = queryBus;
         }
 
-        // GET api/<ShoppingCartController>
+        // POST api/ShoppingCart/product/add
+        [HttpPost("product/add")]
+        public async Task PostAddProduct([FromBody] ProductDTO productDTO)
+        {
+            AddProductCommand command = new AddProductCommand { ProductDTO = productDTO };
+            await commandBus.SendAsync(command);
+        }
+
+        // POST api/ShoppingCart/product/delete
+        [HttpPost("product/delete")]
+        public async Task PostDeleteProduct([FromBody] ProductDTO productDTO)
+        {
+            DeleteProductCommand command = new DeleteProductCommand { ProductDTO = productDTO };
+            await commandBus.SendAsync(command);
+        }
+
+        // GET api/ShoppingCart/print/{shoppingCartId}
         [HttpGet("print/{shoppingCartId}")]
         public async Task<string> Print(string shoppingCartId)
         {
-            //PrintShoppingCartUseCase printShoppingCartUseCase = new PrintShoppingCartUseCase(shoppingCartRepository);
-
-            //PrintShoppingCartRequest printShoppingCartRequest = new PrintShoppingCartRequest(new ShoppingCartDTO { ShoppingCartId = shoppingCartId });
             PrintShoppingCartQuery query = new PrintShoppingCartQuery { ShoppingCartId = shoppingCartId };
-
-            return await queryBus.SendAsync<PrintShoppingCartQuery,string>(query);
-            
-            //return await printShoppingCartUseCase.ExecuteAsync(printShoppingCartRequest);
+            return await queryBus.SendAsync<PrintShoppingCartQuery, string>(query);
         }
-
-        //// POST api/<ShoppingCartController>
-        [HttpPost]
-        public void PostAddProduct([FromBody] ProductDTO productDTO)
-        {
-            IProductRepository productRepository = new ProductRepository(context);
-            AddProductUseCase addProductUseCase = new AddProductUseCase(productRepository, shoppingCartRepository);
-
-            AddProductRequest productRequest = new AddProductRequest(productDTO);
-            addProductUseCase.ExecuteAsync(productRequest);
-        }
-
-        //// POST api/<ShoppingCartController>
-        //[HttpPost]
-        //public void PostApplyDiscount([FromBody] DiscountDTO discountDTO)
-        //{
-        //    IDiscountRepository discountRepository = null;
-        //    ApplyDiscountUseCase applyDiscountUseCase = new ApplyDiscountUseCase(discountRepository, shoppingCartRepository);
-
-        //    DiscountRequest discountRequest = new DiscountRequest(discountDTO);
-        //    applyDiscountUseCase.Execute(discountRequest);
-        //}
-
-        //// DELETE api/<ShoppingCartController>
-        //[HttpDelete]
-        //public void DeleteItem([FromBody] ProductDTO productDTO)
-        //{
-        //    IProductRepository productRepository = new ProductRepository(context);
-        //    DeleteProductUseCase deleteProductUseCase = new DeleteProductUseCase(productRepository, shoppingCartRepository);
-
-        //    DeleteProductRequest deleteProductRequest = new DeleteProductRequest(productDTO);
-        //    deleteProductUseCase.Execute(deleteProductRequest);
-        //}
     }
 }
