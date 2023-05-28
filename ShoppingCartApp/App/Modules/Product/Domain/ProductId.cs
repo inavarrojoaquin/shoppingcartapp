0300d69658fd3f1a6653a9dfd0505636a5034e9b
Marcos <marcos.graup@gmail.com>
2023-05-28 16:59:45 +0200

    feat: add products context

---
+++ ShoppingCartApp/App/Modules/Product/Domain/ProductId.cs
@@ -0,0 +1,26 @@
+﻿namespace ShoppingCartApp.App.Modules.Product.Domain
+{
+    public class ProductId
+    {
+        private readonly Guid guid;
+
+        public ProductId(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new Exception(string.Format("Error: {0} can not be null or empty", "ProductId"));
+
+            this.guid = Guid.ParseExact(id, "D");
+        }
+
+        public static ProductId Create()
+        {
+            return new ProductId(Guid.NewGuid().ToString());   
+        }
+
+        public string Value()
+        {
+            return this.guid.ToString();
+        }
+        
+    }
+}
