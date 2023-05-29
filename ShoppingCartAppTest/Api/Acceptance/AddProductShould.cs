87d5b02c70894bf6663bdcb719f714166d24125f
Joaquin Navarro <inavarrojoaquin@gmail.com>
2023-05-29 18:26:33 +0200

    fix: Routing and async for acceptance tests

--- ShoppingCartAppTest/Api/Acceptance/AddProductShould.cs
+++ ShoppingCartAppTest/Api/Acceptance/AddProductShould.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
-using ShoppingCartApp.App.Domain;
 using ShoppingCartApp.App.UseCases.AddProduct;
-using ShoppingCartApp.Shared.Domain;
 using ShoppingCartApp.Shared.UseCases;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShoppingCartAppTest.Api.Acceptance
 {
@@ -21,17 +14,17 @@ namespace ShoppingCartAppTest.Api.Acceptance
         {
             IBaseUseCase<AddProductRequest> addProductRequest = Substitute.For<IBaseUseCase<AddProductRequest>>();
 
-            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => 
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
             {
-                builder.ConfigureServices(services => { services.AddTransient<IBaseUseCase<AddProductRequest>>(x => addProductRequest); }); 
+                builder.ConfigureServices(services => { services.AddTransient<IBaseUseCase<AddProductRequest>>(x => addProductRequest); });
             });
 
             var client = application.CreateClient();
             var newProduct = new { ProductId = Guid.NewGuid().ToString(), ShoppingCartId = Guid.NewGuid().ToString() };
 
-            var response = await client.PostAsJsonAsync("api/AddProduct", newProduct);
+            var response = await client.PostAsJsonAsync("api/ShoppingCart/product/add", newProduct);
 
-            addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct.ProductId
+            await addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct.ProductId
                                                                                  && x.ShoppingCartId.Value() == newProduct.ShoppingCartId));
         }
 
@@ -46,18 +39,18 @@ namespace ShoppingCartAppTest.Api.Acceptance
             });
 
             var client = application.CreateClient();
-            
+
             var newProduct1 = new { ProductId = Guid.NewGuid().ToString(), ShoppingCartId = Guid.NewGuid().ToString() };
-            var response1 = await client.PostAsJsonAsync("api/AddProduct", newProduct1);
+            var response1 = await client.PostAsJsonAsync("api/ShoppingCart/product/add", newProduct1);
 
             var newProduct2 = new { ProductId = Guid.NewGuid().ToString(), ShoppingCartId = Guid.NewGuid().ToString() };
-            var response2 = await client.PostAsJsonAsync("api/AddProduct", newProduct2);
+            var response2 = await client.PostAsJsonAsync("api/ShoppingCart/product/add", newProduct2);
 
-            addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct1.ProductId
-                                                                                 && x.ShoppingCartId.Value() == newProduct1.ShoppingCartId));
+            await addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct1.ProductId
+                && x.ShoppingCartId.Value() == newProduct1.ShoppingCartId));
 
-            addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct2.ProductId
-                                                                                 && x.ShoppingCartId.Value() == newProduct2.ShoppingCartId));
+            await addProductRequest.Received(1).ExecuteAsync(Arg.Is<AddProductRequest>(x => x.ProductId.Value() == newProduct2.ProductId
+                && x.ShoppingCartId.Value() == newProduct2.ShoppingCartId));
 
         }
     }
